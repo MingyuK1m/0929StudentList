5bddf5039ec6b7aeb664dfa9907e7cb2c58122f6
201412357 <201412357@DESKTOP-LJ6PHG4>
2017-09-29 10:13:26 +0900

    종합예제 완성

---
+++ 201412357-StudentList/src/main/java/com/dit/student/domain/StudentVO.java
@@ -0,0 +1,17 @@
+package com.dit.student.domain;
+public class StudentVO {
+ private String number;
+ private String name;
+ public String getNumber() {
+  return number;
+ }
+ public void setNumber(String number) {
+  this.number = number;
+ }
+ public String getName() {
+  return name;
+ }
+ public void setName(String name) {
+  this.name = name;
+ }
+}
